45109362334473dc0033a75389a5487bcb8325ed
elif0zgnr <elif0zgnr96@hotmail.com>
2018-12-06 22:55:29 +0300

    Complete Project.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        public static int KirmiziIsikSuresi = 20;
+        public static int SariIsikSuresi = 10;
+        public static int YesilIsikSuresi = 20;
+    }
+}
---
+++ Frm_Ayarlar.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Frm_Ayarlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Nud_Kirmizi = new System.Windows.Forms.NumericUpDown();
+            this.Nud_Yesil = new System.Windows.Forms.NumericUpDown();
+            this.Nud_Sari = new System.Windows.Forms.NumericUpDown();
+            this.Btn_Kaydet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Kirmizi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Yesil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Sari)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kırmızı Işık Bekleme Süresi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sarı Işık Bekleme Süresi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yeşil Işık Bekleme Süresi:";
+            // 
+            // Nud_Kirmizi
+            // 
+            this.Nud_Kirmizi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Nud_Kirmizi.Location = new System.Drawing.Point(213, 39);
+            this.Nud_Kirmizi.Name = "Nud_Kirmizi";
+            this.Nud_Kirmizi.Size = new System.Drawing.Size(70, 23);
+            this.Nud_Kirmizi.TabIndex = 3;
+            // 
+            // Nud_Yesil
+            // 
+            this.Nud_Yesil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Nud_Yesil.Location = new System.Drawing.Point(213, 98);
+            this.Nud_Yesil.Name = "Nud_Yesil";
+            this.Nud_Yesil.Size = new System.Drawing.Size(70, 23);
+            this.Nud_Yesil.TabIndex = 4;
+            // 
+            // Nud_Sari
+            // 
+            this.Nud_Sari.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Nud_Sari.Location = new System.Drawing.Point(213, 69);
+            this.Nud_Sari.Name = "Nud_Sari";
+            this.Nud_Sari.Size = new System.Drawing.Size(70, 23);
+            this.Nud_Sari.TabIndex = 5;
+            // 
+            // Btn_Kaydet
+            // 
+            this.Btn_Kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btn_Kaydet.Location = new System.Drawing.Point(272, 137);
+            this.Btn_Kaydet.Name = "Btn_Kaydet";
+            this.Btn_Kaydet.Size = new System.Drawing.Size(75, 31);
+            this.Btn_Kaydet.TabIndex = 6;
+            this.Btn_Kaydet.Text = "Kaydet";
+            this.Btn_Kaydet.UseVisualStyleBackColor = true;
+            this.Btn_Kaydet.Click += new System.EventHandler(this.Btn_Kaydet_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(289, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "sn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(289, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "sn";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(289, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "sn";
+            // 
+            // Frm_Ayarlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 197);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Btn_Kaydet);
+            this.Controls.Add(this.Nud_Sari);
+            this.Controls.Add(this.Nud_Yesil);
+            this.Controls.Add(this.Nud_Kirmizi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Frm_Ayarlar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ayarlar";
+            this.Load += new System.EventHandler(this.Frm_Ayarlar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Kirmizi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Yesil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nud_Sari)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown Nud_Kirmizi;
+        private System.Windows.Forms.NumericUpDown Nud_Yesil;
+        private System.Windows.Forms.NumericUpDown Nud_Sari;
+        private System.Windows.Forms.Button Btn_Kaydet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
